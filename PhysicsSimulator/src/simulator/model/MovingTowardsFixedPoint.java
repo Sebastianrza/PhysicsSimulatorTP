ecbbbb5909fe9bdc3d88b819ef817acf60f1b2ca
Sebastianrza <Sebastianrza@localhost>
2021-02-19 16:29:06 +0100

    Leyes de gravedad agregadas

---
+++ PhysicsSimulator/src/simulator/model/MovingTowardsFixedPoint.java
@@ -0,0 +1,17 @@
+package simulator.model;
+
+import java.util.List;
+
+public class MovingTowardsFixedPoint implements ForceLaws {
+
+	protected double gc = 9.81;
+	@Override
+	public void apply(List<Body> bs) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < bs.size(); i++) {
+			Body bo = bs.get(i);
+			bo.setAcceleration(bo.getPosition().direction().scale(gc));
+		}
+	}
+
+}
