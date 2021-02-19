037229d2adf4e891794a8741cf96c38c6ced6a17
Sebastianrza <Sebastianrza@localhost>
2021-02-19 14:18:17 +0100

    Agregado de algunas clases

--- PhysicsSimulator/src/simulator/model/Body.java
+++ PhysicsSimulator/src/simulator/model/Body.java
@@ -1,7 +1,74 @@
 package simulator.model;
 
-public class Body {//clase cuerpo
+import org.json.JSONObject;
 
+import simulator.misc.Vector2D;
 
+public class Body {
+	
+	protected String id;
+	protected Vector2D force;
+	protected Vector2D position;
+	protected Vector2D velocity;
+	protected double mass;
+	
+	//Constructor
+	public Body(String id, Vector2D position, Vector2D velocity, Double mass) {
+		this.id = id;
+		this.position = position;
+		this.velocity = velocity;
+		this.mass = mass;
+	}
+	//Getters and Setters
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Vector2D getForce() {
+		return force;
+	}
+	public void setForce(Vector2D force) {
+		this.force = force;
+	}
+	public Vector2D getPosition() {
+		return position;
+	}
+	public void setPosition(Vector2D position) {
+		this.position = position;
+	}
+	public Vector2D getVelocity() {
+		return velocity;
+	}
+	public void setVelocity(Vector2D velocity) {
+		this.velocity = velocity;
+	}
+	public double getMass() {
+		return mass;
+	}
+	public void setMass(double mass) {
+		this.mass = mass;
+	}
+	//Metodos
+	
+	void addForce(Vector2D f) {
+		this.force.plus(f);
+	}
+	void resetForce() {
+	
+	}
+	void move(double t) {
+		
+	}
+	
+	public JSONObject getState() {
+		return null;
+		
+	}
+	public String toString() {
+		return id;
+		
+	}
 }
 
---
+++ PhysicsSimulator/src/simulator/model/MassLossingBody.java
@@ -0,0 +1,25 @@
+package simulator.model;
+
+import simulator.misc.Vector2D;
+
+public class MassLossingBody extends Body {
+	
+	protected double lossFactor;
+	protected double lossFrequency;
+	protected double contador;
+
+	public MassLossingBody(double lossFactor, double lossFrequency,String id, Vector2D position, Vector2D velocity, double mass) {
+		super(id, position, velocity, mass);
+		// TODO Auto-generated constructor stub
+		
+		if(this.lossFactor >=0 && this.lossFactor <=1) {
+			this.lossFactor = lossFactor;
+		}
+		if(this.lossFrequency>=0) {
+			this.lossFrequency = lossFrequency;
+		}
+		this.contador = contador;
+	}
+	
+
+}
