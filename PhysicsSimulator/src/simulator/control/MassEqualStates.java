1db9a1e29f6c0942f7c0724e1e27846d1e08d5f8
Sebastianrza <Sebastianrza@localhost>
2021-02-21 14:16:33 +0100

    Clase Epsilon y implementando la MassEqualStates

--- PhysicsSimulator/src/simulator/control/MassEqualStates.java
+++ PhysicsSimulator/src/simulator/control/MassEqualStates.java
@@ -2,8 +2,14 @@ package simulator.control;
 
 import org.json.JSONObject;
 
+import simulator.model.ForceLaws;
+import simulator.model.PhysicsSimulator;
+
 public class MassEqualStates implements StateComparator {
 	
+	protected double time;
+	protected ForceLaws fl;
+	protected PhysicsSimulator ps = new PhysicsSimulator(null, null);
 
 	@Override
 	public boolean equal(JSONObject s1, JSONObject s2) {
--- PhysicsSimulator/src/simulator/factories/NoForceBuilder.java
+++ PhysicsSimulator/src/simulator/factories/NoForceBuilder.java
@@ -1,4 +1,4 @@
-package simulator.control;
+package simulator.factories;
 
 public class NoForceBuilder {
 
