beb1fd1bccb7280a59524b52d8157bb5707537b9
Sebastianrza <Sebastianrza@localhost>
2021-02-23 20:07:52 +0100

    revisar no estoy seguro

--- PhysicsSimulator/src/simulator/factories/MovingTowardsFixedPointBuilder.java
+++ PhysicsSimulator/src/simulator/factories/MovingTowardsFixedPointBuilder.java
@@ -2,7 +2,9 @@ package simulator.factories;
 
 import org.json.JSONObject;
 
+import simulator.misc.Vector2D;
 import simulator.model.ForceLaws;
+import simulator.model.MovingTowardsFixedPoint;
 
 public class MovingTowardsFixedPointBuilder extends Builder<ForceLaws>{
 
@@ -14,7 +16,8 @@ public class MovingTowardsFixedPointBuilder extends Builder<ForceLaws>{
 	@Override
 	protected ForceLaws createTheInstance(JSONObject data) {
 		// TODO Auto-generated method stub
-		return null;
+		Vector2D mtfp = new Vector2D();
+		return new MovingTowardsFixedPoint(mtfp, 9.81);
 	}
 
 }
