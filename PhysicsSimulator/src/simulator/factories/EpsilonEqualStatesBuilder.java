2c4a40e578903993cbbf15e15bb04feaad14f689
Sebastianrza <Sebastianrza@localhost>
2021-02-23 20:10:36 +0100

    revisar tambien

--- PhysicsSimulator/src/simulator/factories/EpsilonEqualStatesBuilder.java
+++ PhysicsSimulator/src/simulator/factories/EpsilonEqualStatesBuilder.java
@@ -1,5 +1,15 @@
 package simulator.factories;
 
-public class EpsilonEqualStatesBuilder {
+import org.json.JSONObject;
+
+import simulator.control.StateComparator;
+
+public class EpsilonEqualStatesBuilder  extends Builder<StateComparator> {
+
+	@Override
+	protected StateComparator createTheInstance(JSONObject data) {
+		// TODO Auto-generated method stub
+		return new EpsilonEqualStates(0.0);
+	}
 
 }
