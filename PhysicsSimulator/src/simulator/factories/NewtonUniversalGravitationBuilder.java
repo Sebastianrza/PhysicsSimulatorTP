afae81646ec7de0e8929bf0fc80693d579a3e4e0
Sebastianrza <Sebastianrza@localhost>
2021-02-23 20:04:58 +0100

    revisar

--- PhysicsSimulator/src/simulator/factories/NewtonUniversalGravitationBuilder.java
+++ PhysicsSimulator/src/simulator/factories/NewtonUniversalGravitationBuilder.java
@@ -3,9 +3,11 @@ package simulator.factories;
 import org.json.JSONObject;
 
 import simulator.model.ForceLaws;
+import simulator.model.NewtonUniversalGravitation;
 
 public class NewtonUniversalGravitationBuilder extends Builder<ForceLaws>{
-
+	
+	double cons = 6.67E-11;
 	public NewtonUniversalGravitationBuilder() {
 		
 		// TODO Auto-generated constructor stub
@@ -14,7 +16,7 @@ public class NewtonUniversalGravitationBuilder extends Builder<ForceLaws>{
 	@Override
 	protected ForceLaws createTheInstance(JSONObject data) {
 		// TODO Auto-generated method stub
-		return null;
+		return new NewtonUniversalGravitation(cons);
 	}
 
 }
