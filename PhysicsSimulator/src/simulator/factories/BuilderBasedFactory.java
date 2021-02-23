bba129694391ac263d5c2dc0c0c68fdd225de9e6
Sebastianrza <Sebastianrza@localhost>
2021-02-23 19:55:20 +0100

    agregue ejemplo no esta terminado

--- PhysicsSimulator/src/simulator/factories/BuilderBasedFactory.java
+++ PhysicsSimulator/src/simulator/factories/BuilderBasedFactory.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.json.JSONObject;
 
+import simulator.model.Body;
+
 public class BuilderBasedFactory<T> implements Factory<T> {
 
 	private List<Builder<T>> builders;
@@ -15,13 +17,16 @@ public class BuilderBasedFactory<T> implements Factory<T> {
 	}
 	@Override
 	public T createInstance(JSONObject info) {
-		// TODO Auto-generated method stub
+	/*	ArrayList<Builder<Body>> bodyBuilders = new ArrayList<>();
+		bodyBuilders.add(new BasicBodyBuilder());
+		bodyBuilders.add(new MassLosingBodyBuilder());
+		Factory<Body> bodyFactory = new BuilderBasedFactory<Body>(bodyBuilders);*/
 		return null;
 	}
 
 	@Override
 	public List<JSONObject> getInfo() {
-		// TODO Auto-generated method stub
+	
 		return null;
 	}
 
