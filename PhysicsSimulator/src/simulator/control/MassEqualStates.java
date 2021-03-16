778f565eedb6347bbb9d9e619d66bd945ec44ae9
Sebastian <sezambra@ucm.es>
2021-03-16 15:27:36 +0100

    massequal

--- PhysicsSimulator/src/simulator/control/MassEqualStates.java
+++ PhysicsSimulator/src/simulator/control/MassEqualStates.java
@@ -3,11 +3,11 @@ package simulator.control;
 import org.json.JSONObject;
 
 public class MassEqualStates implements StateComparator {
-
+	
 	@Override
 	public boolean equal(JSONObject s1, JSONObject s2) {
 		
-		if(s1.get("time") == s2.get("time")){
+		if(s1.get("time") == s2.get("time") && s1.getJSONArray("bodies").length() == s2.getJSONArray("bodies").length()){
 			
 			for (int i=0; i < s1.getJSONArray("bodies").length(); i++) {
 				
