cef2fbdd3affbbd7d34901e47ed60ee31b5e737e
Sebastian Zambrano <45112547+Sebastianrza@users.noreply.github.com>
2021-05-15 11:51:59 +0200

    boton fl listo, falta su clase

--- PhysicsSimulator/src/views/ForceLawsDialog.java
+++ PhysicsSimulator/src/views/ForceLawsDialog.java
@@ -1,9 +1,17 @@
 package views;
 
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+import simulator.control.Controller;
 
 public class ForceLawsDialog extends JDialog{
 
+	private static final long serialVersionUID = 1L;
+	
+	public ForceLawsDialog(Controller ctrl, JFrame jFrame) {
+		
+	}
 	
 }
